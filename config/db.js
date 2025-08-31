3705497aa21fed205ec59f86dbcae4d613dedd67
Mahima Patel <mahimapatel7016@gmail.com>
2025-08-31 16:42:29 +0530

    Add comments to Dao + DB Updation

--- config/db.js
+++ config/db.js
@@ -3,7 +3,5 @@ import 'dotenv/config'
 
 export const connectDB = async () => {
     const mongo_url = process.env.MONGODB_URL
-
     await mongoose.connect(mongo_url).then(()=>console.log("DB Connected"));
 }
-
--- dao/movieDao.js
+++ dao/movieDao.js
@@ -1,18 +1,22 @@
 import Movie from "../models/movieModel.js";
 
+// Create new movie
 export const createMovie = async (movieData) => {
   const movie = new Movie(movieData);
   return await movie.save();
 };
 
+// Get all movies
 export const getAllMovies = async () => {
   return await Movie.find();
 };
 
+// Get movie by ID
 export const getMovieById = async (id) => {
   return await Movie.findById(id);
 };
 
+// Update movie
 export const updateMovie = async (id, movieData) => {
   return await Movie.findByIdAndUpdate(id, movieData, {
     new: true,
@@ -20,7 +24,7 @@ export const updateMovie = async (id, movieData) => {
   });
 };
 
+// Delete movie
 export const deleteMovie = async (id) => {
   return await Movie.findByIdAndDelete(id);
 };
-
